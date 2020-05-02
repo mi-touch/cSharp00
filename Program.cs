4cbaf4b69cefaaf2b583b34f18ec0d600f75deb0
mi-touch <timileyinadefare@yahoo.com>
2020-05-02 17:12:36 +0100

    Declaring variable and data types

--- Program.cs
+++ Program.cs
@@ -7,6 +7,17 @@ namespace learnFromHome
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            // How to declare variable
+            int var1 = 225;
+            double stat = 88.999;
+            char xyz = 'G';
+            string name = "Goodness";
+            // Printing to the console
+            Console.WriteLine(var1);
+            Console.WriteLine(name);
+            Console.WriteLine(xyz);
+            Console.WriteLine(stat);
+            Console.ReadKey();
         }
     }
 }
